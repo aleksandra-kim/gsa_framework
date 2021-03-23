5f4a9aca97f24371a8534805243a6027bc57d4bf
aleksandra-kim <aleksandra.kim@icloud.com>
2021-03-23 19:04:17 +0100

    sync from local

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../../gsa_framework"))
+sys.path.insert(0, os.path.abspath("../.."))
 
 
 # -- Project information -----------------------------------------------------
