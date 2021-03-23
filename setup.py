be85638b7eb9fab1e172b3e7606e2b170daf1f76
aleksandra-kim <aleksandra.kim@icloud.com>
2021-03-23 18:28:33 +0100

    sync from local

--- setup.py
+++ setup.py
@@ -6,15 +6,30 @@ with open("gsa_framework/version.py") as fp:
     exec(fp.read(), v_temp)
 version = ".".join((str(x) for x in v_temp["version"]))
 
+packages = []
+root_dir = os.path.dirname(".")
+if root_dir:
+    os.chdir(root_dir)
+
+# Probably should be changed, __init__.py is no longer required for Python 3
+for dirpath, dirnames, filenames in os.walk("gsa_framework"):
+    # Ignore dirnames that start with '.'
+    if "__init__.py" in filenames:
+        pkg = dirpath.replace(os.path.sep, ".")
+        if os.path.altsep:
+            pkg = pkg.replace(os.path.altsep, ".")
+        packages.append(pkg)
+
 
 setup(
     name="gsa_framework",
     version=version,
-    packages=[
-        "gsa_framework",
-        "gsa_framework.sampling",
-        "gsa_framework.sensitivity_methods",
-    ],
+    # packages=[
+    #     "gsa_framework",
+    #     "gsa_framework.sampling",
+    #     "gsa_framework.sampling.get_samples",
+    #     "gsa_framework.sensitivity_methods",
+    # ],
     author="Aleksanda Kim",
     author_email="aleksandra.kim@icloud.com",
     license="BSD-3-Clause",
@@ -28,6 +43,8 @@ setup(
         "scikit-learn",
         "scipy",
         "xgboost",
+        "pre-commit",
+        "sphinxcontrib-bibtex",
     ],
     url="https://github.com/aleksandra-kim/gsa_framework",
     long_description_content_type="text/markdown",
